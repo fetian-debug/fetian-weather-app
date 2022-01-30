b15adbf5bcc8edfa3824fe54213ce2da4f4270c5
fetian ans <fetianans@gmail.com>
2022-01-30 19:08:10 +0200

    updates

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -3,6 +3,7 @@ const request = require('request')
 const geocode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiZmV0aWFuIiwiYSI6ImNrejFnaWNtaDFsdW0yd3Fsd3E4dGZhN3MifQ.W0F65qGEN_rWVUsIH2myQQ'
 
+ 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to location services!', undefined)
