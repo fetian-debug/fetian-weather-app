371e965f405cc5a1f519a9e7324eb1ed101a0cdb
fetian ans <fetianans@gmail.com>
2022-01-31 00:41:23 +0200

    updates

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,17 +1,15 @@
-const request = require('request')
-
+const request = require('postman-request');
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/7f313b1021660647fd78530066574f9e/' + latitude + ',' + longitude
-
-    request({ url, json: true }, (error, { body }) => {
-        if (error) {
-            callback('Unable to connect to weather service!', undefined)
-        } else if (body.error) {
-            callback('Unable to find location', undefined)
-        } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. This high today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow + '. There is a ' + body.currently.precipProbability + '% chance of rain.')
-        }
-    })
+  const accessKey = process.env.WEATHER_STACK_ACCESS_KEY
+  const url = `http://api.weatherstack.com/current?access_key=${accessKey}&query=${latitude},${longitude}`
+  request({ url, json: true }, (error, response, body) => {
+    if (error || response.statusCode !== 200) {
+      callback('Unable to connect to weather service!', undefined)
+    } else if (body.error) {
+      callback('Unable to find location', undefined)
+    } else {
+      callback(undefined, `${body.current.weather_descriptions[0]} It is currently ${body.current.temperature} degress out. There is a ${body.current.precip * 100}% chance of rain.`)
+    }
+  })
 }
-
 module.exports = forecast
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,22 +1,25 @@
-const request = require('request')
-
-const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiZmV0aWFuIiwiYSI6ImNrejFnaWNtaDFsdW0yd3Fsd3E4dGZhN3MifQ.W0F65qGEN_rWVUsIH2myQQ&limit=1'
-
- 
-    request({ url, json: true }, (error, { body }) => {
-        if (error) {
-            callback('Unable to connect to location services!', undefined)
-        } else if (body.features.length === 0) {
-            callback('Unable to find location. Try another search.', undefined)
-        } else {
-            callback(undefined, {
-                latitude: body.features[0].center[1],
-                longitude: body.features[0].center[0],
-                location: body.features[0].place_name
-            })
-        }
-    })
+// geocode.js
+const request = require('postman-request');
+const geocode = (address, callback) => {  
+  const accessToken = process.env.MAPBOX_ACCESS_TOKEN
+  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=${accessToken}`
+  request({ url, json: true }, (error, response, body) => {
+    if (error || response.statusCode !== 200) {
+      callback('Unable to connect to location services!', undefined)
+    }
+    else if (body.features.length === 0) {
+      callback('Unable to find location. Try another search.', undefined)
+    }
+    else {
+      const latitude = body.features[0].center[1]
+      const longitude = body.features[0].center[0]
+      const location = body.features[0].place_name
+      callback(undefined, { 
+        latitude, 
+        longitude,
+        location
+      })
+    }
+  }) 
 }
-
 module.exports = geocode
